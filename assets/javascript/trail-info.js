2d21b60c3a9a6050123b57f9c23b323ebac5b99e
Molly Wojcik <mollywojcik@gmail.com>
2018-01-17 19:06:10 -0700

    Added variables for checklist arrays

--- assets/javascript/trail-info.js
+++ assets/javascript/trail-info.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
 
+  // Checklist Arrays
+  var hotChecklist = [];
+  var coldChecklist = [];
+  var rainyChecklist = [];
+  var snowChecklist = [];
 
+
+  // Firebase
+  <script src="https://www.gstatic.com/firebasejs/4.8.2/firebase.js"></script>
+  <script>
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBEwnr_R2pjbyqaKWxKuSyQBtm3LbTdgS4",
+      authDomain: "trailblazer-project.firebaseapp.com",
+      databaseURL: "https://trailblazer-project.firebaseio.com",
+      projectId: "trailblazer-project",
+      storageBucket: "trailblazer-project.appspot.com",
+      messagingSenderId: "615321105967"
+    };
+    firebase.initializeApp(config);
+  </script>
 
 
 
